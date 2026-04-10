19dedae181e4b52e40f7102fbb9b7a1b55eeb990
amanbadyal7214 <amanbadyal94@gmail.com>
2026-04-10 13:09:36 +0530

    logo

--- src/components/LogoLoader.tsx
+++ src/components/LogoLoader.tsx
@@ -1,5 +1,5 @@
 import { motion, AnimatePresence } from "framer-motion";
-import logo from "../assets/Logo.png";
+import logo from "../assets/Logo.jpeg";
 import { useEffect, useState } from "react";
 
 export default function LogoLoader({ onComplete }: { onComplete: () => void }) {
